2dd0cad4f3e131de6d49bc956bf27a48ae3fbe92
Jennifer Brown <jm.howardbrown@gmail.com>
2013-07-18 23:52:44 -0400

    removed secret token.
    for security reasons

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Geoapp::Application.config.secret_key_base = '66a1595b9f609686f868a81d3abf45cc2dd5bf766bc3a7a9a19afa4040bfd87447582c03aced9f2c98fcb64558ca109ed69dbe3a3d77d7fb3b4519ca6ea99aa9'
+Geoapp::Application.config.secret_key_base = REMOVED FOR SECURITY RESONS
